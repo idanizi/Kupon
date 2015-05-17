d945def0d3323f6a6e9956c53466de7e3db17cf1
bezen <bigbezen@walla.com>
2015-05-17 23:36:18 +0300

    update

--- Kupon/Kupon_SLN/Kupon_WPF/forms/add/addNewUser.xaml.cs
+++ Kupon/Kupon_SLN/Kupon_WPF/forms/add/addNewUser.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -18,12 +19,12 @@ namespace Kupon_WPF.forms.add
 {
     public partial class addNewUser : Window
     {
-
+        string mode;
         BL server = new BL();
         public addNewUser()
         {
             InitializeComponent();
-
+            mode = "new";
         }
 
 
@@ -40,7 +41,6 @@ namespace Kupon_WPF.forms.add
                     MessageBox.Show("one or more of the parameters is empty!", "error");
                     return false;
                 }
-
                 if (!(Regex.Match(Pass_PB.Password, @"^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{4,8}$")).Success)
                 {
                     MessageBox.Show
@@ -48,18 +48,12 @@ namespace Kupon_WPF.forms.add
                     "and must include at least one upper case letter, one lower case letter, and one numeric digit."
                     , "error");
                 }
-
-
-
-
                 if (!(Regex.Match(Phone_TB.Text, @"(^\+[0-9]{2}|^\+[0-9]{2}\(0\)|^\(\+[0-9]{2}\)\(0\)|^00[0-9]{2}|^0)([0-9]{9}$|[0-9\-\s]{10}$)")).Success)
                 {
                     MessageBox.Show
                         ("the phone number you have entered is not a valied phone number"
                     , "error");
                 }
-
-
                 if (!(Regex.Match(Phone_TB.Text, @"(^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$")).Success)
                 {
                     MessageBox.Show
@@ -67,7 +61,6 @@ namespace Kupon_WPF.forms.add
                     , "error");
                 }
             }
-
              if(!(server.getUser(UserName_TB.Text) == null){
                  MessageBox.Show("user name or mail already exist in the system.", "error");
                  return false;
@@ -89,18 +82,10 @@ namespace Kupon_WPF.forms.add
 
             if (validateFields())
             {
-                Client newUser = new Client(UserName_TB.Text, Pass_PB.Password, Mail_TB.Text, Phone_TB.Text, "todo", "todo2", new List<buisnessCategory>()
-                {
-
-buisnessCategory.Books,
-buisnessCategory.Electronics,
-buisnessCategory.fashion,
-buisnessCategory.Food,
-buisnessCategory.Games,
-buisnessCategory.lifeStile,
-buisnessCategory.Other,
-buisnessCategory.Sports,
-                }, new List<Kupon>(), "todo", "todo", -1);   //TODO  
+                
+                Array arr = Enum.GetValues(typeof(buisnessCategory));
+                  List<buisnessCategory> catList = new List<buisnessCategory>(arr.Cast<buisnessCategory>());
+                Client newUser = new Client(UserName_TB.Text, Pass_PB.Password, Mail_TB.Text, Phone_TB.Text,firstName_TB.Text,lastName_TB.Text, catList, new List<Kupon>(), address_TB.Text, "todo", -1);   //TODO  
                 try
                 {
                     server.addNewUser(newUser);
@@ -132,7 +117,6 @@ buisnessCategory.Sports,
             InitializeComponent();
             mode = "new";
         }
-
         public addPatient(IBase editDoc)
         {
             mode = "edit";
@@ -143,23 +127,17 @@ buisnessCategory.Sports,
                 FNTB.Text = ((patient)editDoc).FirstName;
                 LNTB.Text = ((patient)editDoc).LastName;
                 GCB.Text = ((patient)editDoc).Gender;
-
                 DIDTB.Text = ((patient)editDoc).MainDoctor.ToString();
                 BirthDate_DP.SelectedDate = ((patient)editDoc).DateBirth;
                 pass_PB.Password = ((patient)editDoc).Password;
             }
         }
-
-
         private void Button_Click(object sender, RoutedEventArgs e)
         {
             
             if (commonMethods.isNumber(IDTB.Text) && commonMethods.validNameCheck(FNTB.Text) && commonMethods.validNameCheck(LNTB.Text) && commonMethods.isNumber(DIDTB.Text) && BirthDate_DP.SelectedDate != null)
             {
-
-
                 patient newPat = new patient(Convert.ToInt32(IDTB.Text), FNTB.Text, LNTB.Text, GCB.Text, Convert.ToInt32(DIDTB.Text), (DateTime)BirthDate_DP.SelectedDate, pass_PB.Password);
-
                 string wrong = "";
                 if (mode == "edit")
                 {
@@ -197,7 +175,6 @@ buisnessCategory.Sports,
              
         
         }
-
          private void FNTB_LostFocus(object sender, RoutedEventArgs e)
         {
             
@@ -206,7 +183,6 @@ buisnessCategory.Sports,
                 MessageBox.Show("illegale first name, please try again", "error");
             }
         }
-
         private void LNTB_LostFocus(object sender, RoutedEventArgs e)
         {
             if (!commonMethods.validNameCheck(LNTB.Text))
@@ -214,7 +190,6 @@ buisnessCategory.Sports,
                 MessageBox.Show("illegale last name, please try again", "error");
             }
         }
-
         private void IDTB_LostFocus(object sender, RoutedEventArgs e)
         {
             if (commonMethods.isNumber(IDTB.Text))
@@ -227,7 +202,6 @@ buisnessCategory.Sports,
             }
             else MessageBox.Show("illegale ID,please try again", "error");
         }
-
         private void DIDTB_LostFocus(object sender, RoutedEventArgs e)
         {
             if (commonMethods.isNumber(DIDTB.Text))
@@ -240,22 +214,18 @@ buisnessCategory.Sports,
             }
             else MessageBox.Show("illegale doctor's ID,please try again", "error");
         }
-
         private void FNTB_GotFocus(object sender, RoutedEventArgs e)
         {
             FNTB.Text = "";
         }
-
         private void LNTB_GotFocus(object sender, RoutedEventArgs e)
         {
             LNTB.Text = "";
         }
-
         private void IDTB_GotFocus(object sender, RoutedEventArgs e)
         {
             IDTB.Text = "";
         }
-
           private void DIDTB_GotFocus(object sender, RoutedEventArgs e)
         {
             DIDTB.Text = "";
@@ -265,6 +235,3 @@ buisnessCategory.Sports,
  
 
  
-
-      
-                
